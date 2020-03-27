a3161acb5fff4c9ca7c28272be28d9e66f702433
Faizal Thamrin <thamrinf@gmail.com>
2020-03-27 15:55:40 +0700

    version1

---
+++ src/js/map.js
@@ -0,0 +1,105 @@
+// INITIAL VARIABLE
+var yemen_hno,
+  theDataTableInit = 0,
+  human_number = d3.format(",d"),
+  SelectedRegion = "adm1name",
+  statesData = 0,
+  inneed = 0,
+  inneed_acute_pct = 0,
+  inneed_moderate_pct = 0,
+  population = 0;
+
+var geojson;
+var map = L.map("map_div", {
+  //map is a global variable
+  center: [15.6999243, 47.8915271],
+  zoom: 7,
+  zoomDelta: 0.1,
+  zoomSnap: 0,
+  zoomControl: false,
+  layerControl: false,
+  attributionControl: true,
+  scrollWheelZoom: false,
+  minZoom: 7,
+  maxZoom: 7
+});
+var gv_baseMap = L.mapboxGL({
+  accessToken:
+    "pk.eyJ1IjoidGhhbXJpbmYiLCJhIjoiYzA1MmJjMzI1N2E5NzNhN2I2MzU4MDkzZWU4ODQxNzAifQ.3qQApYaqLA0bGC3Z5PCnUg",
+  style: "mapbox://styles/thamrinf/ck10tppqm034k1cr0bu9mhdpl",
+  zIndex: 1
+});
+
+var humanNumber = d3.format(",d");
+
+gv_baseMap.addTo(map);
+
+$("#loadinggif").show();
+d3.json("data/yem_admbnda_adm1.geojson").then(function(geodata) {
+  function getColor(d) {
+    return d > 1000
+      ? "#800026"
+      : d > 500
+      ? "#BD0026"
+      : d > 200
+      ? "#E31A1C"
+      : d > 100
+      ? "#FC4E2A"
+      : d > 50
+      ? "#FD8D3C"
+      : d > 20
+      ? "#FEB24C"
+      : d > 10
+      ? "#FED976"
+      : "#FFEDA0";
+  }
+
+  function style(feature) {
+    return {
+      weight: 2,
+      opacity: 1,
+      color: "white",
+      dashArray: "3",
+      fillOpacity: 0.7,
+      fillColor: getColor(feature.properties.density)
+    };
+  }
+
+  function highlightFeature(e) {
+    var layer = e.target;
+
+    layer.setStyle({
+      weight: 5,
+      color: "#666",
+      dashArray: "",
+      fillOpacity: 0.7
+    });
+
+    if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+      layer.bringToFront();
+    }
+  }
+
+  var geojson;
+
+  function resetHighlight(e) {
+    geojson.resetStyle(e.target);
+  }
+
+  function zoomToFeature(e) {
+    map.fitBounds(e.target.getBounds());
+  }
+
+  function onEachFeature(feature, layer) {
+    layer.on({
+      mouseover: highlightFeature,
+      mouseout: resetHighlight,
+      click: zoomToFeature
+    });
+  }
+
+  geojson = L.geoJson(geodata, {
+    style: style,
+    onEachFeature: onEachFeature
+  }).addTo(map);
+});
